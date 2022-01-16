c8eece699feef86430e8417aca79352bdeb5228a
coseres <coseresmauricio@gmail.com>
2022-01-16 09:23:06 -0300

    desafio entregable

---
+++ desafio.js
@@ -0,0 +1,47 @@
+
+let repetir = prompt("¿cuantas veces quieres repetir el mensaje?")
+
+let x = 1
+
+for(x; x<=repetir; x++){
+    console.log("Hola " + x)
+}
+
+
+
+
+/* Sumar un número a otro determinada cantidad de veces */
+
+
+
+/* let numero = Number(prompt("Ingrese un número"))
+
+let sumar = Number(prompt("¿Que numero quieres sumarle?"))
+
+let repetir = Number(prompt("¿Cuantas veces lo quiere sumar?"))
+
+let x = 0
+
+for(x; x <= repetir; x++){
+    resultado = numero + (sumar*x)
+    alert (resultado)
+} */
+ 
+
+
+/* Concatenar texto hasta quediga esc */
+
+
+
+/* let palabra = ""
+let frase = ""
+
+while (palabra != "esc"){
+    frase += palabra + " "
+    palabra = prompt("Ingresa una palabra o esc para finalizar")
+    
+}
+console.log(frase) */
+
+
+
